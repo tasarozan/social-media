9d45543bd6e6b95bcddbe9eb186b3ad17868dda6
Ozan <tasarozan@gmail.com>
2020-12-29 23:37:53 +0300

    Implement promises

--- week-3/index.js
+++ week-3/index.js
@@ -1,57 +1,58 @@
-  // const User = require('./user')
-  // const Tweet = require('./tweet')
-  // const Chalk = require('chalk')
-  const Database = require('./database')
-  // const User = require('./user')
-  const afterLoadingDatabase = (err, loadedFile) => {
-    if(err) {
-      console.log('There is an error', err)
-      return
-    }
-    loadedFile.forEach(x => console.log(x.username))
+const User = require('./models/user')
+const Tweet = require('./models/tweet')
+const UserService = require('./services/user-service')
+const TweetService = require('./services/tweet-service')
+
+const afterLoadingDatabase = (err, loadedFile) => {
+  if (err) {
+    console.log('There is an error', err)
+    return
   }
- 
-    
-  //  const ozan = new User('cat47lyst', 'Ozan TASAR')
-  //  const berk = new User('berkXD', 'Berk USLU')
-  //  const furkan = new User('furkanradiela', 'Furkan İLGENCİ')
-  //  const mertcan = User.create({username:'mertcanpsycho', name:'Mert Can Gökdağ'})
-  
-  
-   
-     
-  //  furkan.sendTweet('SELAM 🍻')
-  //  furkan.sendTweet('Sorry')
-  //  ozan.sendTweet('SIL BUNU')
-  //  berk.sendTweet('BUNE OLM')
-  //  mertcan.sendTweet('S.A')
-  
-  //  berk.follow(ozan)
-  //  berk.follow(furkan)
-  //  furkan.follow(ozan)
-  //  ozan.follow(furkan)
-  //  ozan.follow(mertcan)
-  
-  //  ozan.likeTweet(furkan.tweets[0])
-  //  berk.likeTweet(furkan.tweets[0])
-  //  furkan.likeTweet(furkan.tweets[0])
-  //  ozan.likeTweet(mertcan.tweets[0])
-   
+  loadedFile.forEach(x => console.log(x.username))
+}
 
-  //  ozan.retweet(furkan.tweets[0])
+async function main() {
 
-  //  ozan.unfollow(furkan)
+  const ozan = new User('cat47lyst', 'Ozan TASAR')
+  const berk = new User('berkXD', 'Berk USLU')
+  const furkan = new User('furkanradiela', 'Furkan İLGENCİ')
+  const mertcan = User.create({ username: 'mertcanpsycho', name: 'Mert Can Gökdağ' })
+
+  await UserService.add(ozan)
+  await UserService.add(berk)
+  await UserService.add(furkan)
+  await UserService.add(mertcan)
+
+  const people = await UserService.findAll()
+
+  console.log(people[0].name)
+
+  //  await UserService.del(1)
+  furkan.sendTweet('SELAM 🍻')
+  furkan.sendTweet('Sorry')
+  ozan.sendTweet('SIL BUNU')
+  berk.sendTweet('BUNE OLM')
+  mertcan.sendTweet('S.A')
+
+  berk.follow(ozan)
+  berk.follow(furkan)
+  furkan.follow(ozan)
+  ozan.follow(furkan)
+  ozan.follow(mertcan)
+
+  ozan.likeTweet(furkan.tweets[0])
+  berk.likeTweet(furkan.tweets[0])
+  furkan.likeTweet(furkan.tweets[0])
+  ozan.likeTweet(mertcan.tweets[0])
+
+  ozan.retweet(furkan.tweets[0])
   
-  //  Database.save('user.json', [ozan, furkan, berk, mertcan]) 
-  
-  
-  Database.load('user.json', afterLoadingDatabase)
-  
-  
-  
-  
-  
-  
-  
-  
-  
+}
+main()
+
+
+
+
+
+
+
